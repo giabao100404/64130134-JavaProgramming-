52ebf6c1868f201a1b460457b61059ab08c87703
giabao100404 <tbao100404@gmail.com>
2025-02-19 08:14:10 +0700

    tinhkhoilapphuong

---
+++ Lab1Bai3/src/ntu/tlgb/tinhkhoilapphuong.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ntu.tlgb;
+
+/**
+ * 
+ */
+public class tinhkhoilapphuong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
